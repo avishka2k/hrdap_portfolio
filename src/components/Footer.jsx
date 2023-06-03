6e4dee39b4902fe126c0e9ab90ca302136fd073c
avishka <avishka@kali>
2023-06-03 16:00:49 +0530

    base update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {Basic} from "../util/User"
+
+export default function Footer() {
+  return (
+    <div>{Basic.full_name}</div>
+  )
+}
---
+++ src/util/User.jsx
@@ -0,0 +1,20 @@
+export const Basic = {
+  full_name: "Avishka Prabath",
+  email: "avishka2k@gmail.com",
+  Address: "Rathnapura, Sri Lanka",
+};
+
+export const Social = [
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/avishkap",
+  },
+  {
+    name: "Github",
+    url: "https://github.com/avishka2k",
+  },
+  {
+    name: "Facebook",
+    url: "https://www.facebook.com/avishka2k",
+  },
+];
