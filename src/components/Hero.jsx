24ce7ef755f31b0b8097d31abba89d7c59ffb296
A Prabath <avishka2k@gmail.com>
2023-07-13 18:49:04 +0000

    Update 5 files

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { RiFacebookFill } from "react-icons/ri";
 import { FiGithub } from "react-icons/fi";
 import Profile_picture from "../assets/img/Profile_picture.png";
 import { MdDone } from "react-icons/md";
 import { Basic, Social } from "../util/User";
 import { FaLinkedinIn } from "react-icons/fa";
+import Typewriter from "typewriter-effect";
+import AutoIncrement from "./NumberIncrement";
 
 function Hero() {
+  
   return (
     <div className="max-w-screen-xl h-screen flex flex-wrap items-center justify-between mx-auto py-8 px-28">
       <div className="bg-app_primary w-[30rem] relative">
@@ -14,7 +17,9 @@ function Hero() {
         <div className="absolute bottom-0 right-[-3rem] bg-app_black flex items-center gap-5 py-3 px-7 rounded-full">
           <MdDone className=" text-app_primary border-app_primary border-2 rounded-full w-8 h-8 p-1" />
           <div className="">
-            <p className="text-2xl">1100+</p>
+            <p className="text-2xl">
+              <AutoIncrement target={100} />+
+            </p>
             <p className="text-sm">Completed Projects</p>
           </div>
         </div>
@@ -24,13 +29,21 @@ function Hero() {
           Hello i'm
         </p>
         <p className=" text-6xl font-semibold mb-5">{Basic.full_name}</p>
-        <p className="text-3xl font-light mb-5">DevOps Engineer</p>
+        <p className="text-3xl font-light mb-5">
+          <Typewriter
+            options={{
+              strings: ["DevOps Engineer", "App Developer", "Web Developer"],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </p>
         <p className=" text-sm text-app_gray">
           There are many variations of sum available <br /> in the galley of
           type and scrambled it some.
         </p>
         <div className="flex mt-10 items-center">
-          <button className=" border-app_primary border-2 rounded-full py-1 px-4 mr-10 text-app_primary">
+          <button className="border-app_primary border-2 rounded-full py-1 px-4 mr-10 text-app_primary">
             About Me
           </button>
           Follow Me:
